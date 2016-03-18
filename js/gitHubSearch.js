33499e2208900d9c8a858da91fd29cba9e7c4ca6
nic <netzeln@gmail.com>
2016-03-18 11:28:08 -0700

    add ability to list 100 repos

--- js/gitHubSearch.js
+++ js/gitHubSearch.js
@@ -1,12 +1,14 @@
 // var key = require('./../.env').apiKey;
 
 exports.getRepos = function(searchedName, key){
-  $.get('https://api.github.com/users/' + searchedName +'?access_token=' + key).then(function(response){
+  $.get('https://api.github.com/users/' + searchedName +'/repos?page1&per_page=100&access_token=' + key).then(function(response){
     console.log(response);
-  
+  console.log(response[0].name);
+  $('#results_list').empty();
+  for(var i = 0; i <= response.length; i++){
 
-    $('.results').append("<p>Go to the repositories of <a href='" + response.repos_url + "'>" + searchedName + "</a> </p>");
-
+    $('#results_list').append("<li>"+ response[i].name + "</li>");
+  }
   })
   .fail(function(error){
     console.log(error.responseJSON.message);
