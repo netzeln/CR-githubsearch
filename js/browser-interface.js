ef8250ea6d84f3c02dc883debaa02be40825ea19
nic <netzeln@gmail.com>
2016-03-18 10:55:36 -0700

    make a link that isn't a link display on screen

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,25 +1,18 @@
 var apiKey = require('./../.env').apiKey;
-// var getRepos = require('./../js/gitHubSearch.js').getRepos;
+var getRepos = require('./../js/gitHubSearch.js').getRepos;
 
-var getRepos = function(searchedName, key){
-  
-  $.get('https://api.github.com/users/' + searchedName +'?access_token=' + key).then(function(response){
 
-    console.log(response);
-  }).fail(function(error){
-
-    console.log(error.responseJSON.message);
-  });
-};
 
 $(document).ready (function(event){
 
   $('#do_search').click(function(){
     var searchedName = $('input#name_search').val();
 
-  getRepos(searchedName, apiKey);
-
-
+    getRepos(searchedName, apiKey);
+    
   });
 
+
+
+  event.preventDefault;
 });
--- js/gitHubSearch.js
+++ js/gitHubSearch.js
@@ -1,13 +1,15 @@
-// // var key = require('./../.env').apiKey;
-//
-// exports.getRepos = function(searchedName, key){
-//
-//   // console.log('https://api.github.com/users/' + searchedName +'?access_token=' + key);
-//   $.get('https://api.github.com/users/netzeln?access_token=b79bf6c2230d16d634db884c1e9bfd9b429b0e8b').then(function(response){
-//
-//     console.log(response);
-//   }).fail(function(error){
-//
-//     console.log(error.responseJSON.message);
-//   });
-// };
+// var key = require('./../.env').apiKey;
+
+exports.getRepos = function(searchedName, key){
+  $.get('https://api.github.com/users/' + searchedName +'?access_token=' + key).then(function(response){
+    console.log(response);
+  
+
+    $('.results').append("<p>Go to the repositories of <a href='" + response.repos_url + "'>" + searchedName + "</a> </p>");
+
+  })
+  .fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+};
