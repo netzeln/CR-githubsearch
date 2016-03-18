2fe58509ceb7899d165484ef05495d3e1bec542e
nic <netzeln@gmail.com>
2016-03-18 09:25:05 -0700

    GIVE THE TWO FINGER SALUTE TO TYPE=BUTTON

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,6 +1,25 @@
-var gitHubSearch = require'(./../githubsearch.js').gitHubSearch
-var apiKey = require('./../.env').apiKey
+var apiKey = require('./../.env').apiKey;
+// var getRepos = require('./../js/gitHubSearch.js').getRepos;
+
+var getRepos = function(searchedName, key){
+  
+  $.get('https://api.github.com/users/' + searchedName +'?access_token=' + key).then(function(response){
+
+    console.log(response);
+  }).fail(function(error){
+
+    console.log(error.responseJSON.message);
+  });
+};
 
 $(document).ready (function(event){
+
+  $('#do_search').click(function(){
+    var searchedName = $('input#name_search').val();
+
+  getRepos(searchedName, apiKey);
+
+
+  });
 
 });
--- js/gitHubSearch.js
+++ js/gitHubSearch.js
@@ -1,7 +1,13 @@
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
-    console.log(response);
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
-};
+// // var key = require('./../.env').apiKey;
+//
+// exports.getRepos = function(searchedName, key){
+//
+//   // console.log('https://api.github.com/users/' + searchedName +'?access_token=' + key);
+//   $.get('https://api.github.com/users/netzeln?access_token=b79bf6c2230d16d634db884c1e9bfd9b429b0e8b').then(function(response){
+//
+//     console.log(response);
+//   }).fail(function(error){
+//
+//     console.log(error.responseJSON.message);
+//   });
+// };
