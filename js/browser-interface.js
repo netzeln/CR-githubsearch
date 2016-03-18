1d9ab3a67c039dc0471325ac18c9efdc850e7ae5
nic <netzeln@gmail.com>
2016-03-18 14:18:12 -0700

    add a button that hides and a user display

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,15 +1,21 @@
 var apiKey = require('./../.env').apiKey;
 var getRepos = require('./../js/gitHubSearch.js').getRepos;
-
+var getProfile = require('./../js/gitHubSearch.js').getProfile;
 
 
 $(document).ready (function(event){
-
-  $('#do_search').click(function(){
+  $('#user_search').click(function(){
     var searchedName = $('input#name_search').val();
 
+    getProfile(searchedName, apiKey);
+    $(".results").show();
+  });
+
+  $('#repo_search').click(function(){
+    var searchedName = $('input#name_search').val();
+    $("#repo_search").hide();
     getRepos(searchedName, apiKey);
-    
+
   });
 
 
--- js/gitHubSearch.js
+++ js/gitHubSearch.js
@@ -2,11 +2,11 @@
 
 exports.getRepos = function(searchedName, key){
   $.get('https://api.github.com/users/' + searchedName +'/repos?page1&per_page=100&access_token=' + key).then(function(response){
-    console.log(response);
-  console.log(response[0].name);
+
+
   $('#results_list').empty();
   for(var i = 0; i <= response.length; i++){
-    if(response[i].description != "")
+    if(response[i].description !== "")
     {
       var description = response[i].description;
     } else {
@@ -15,6 +15,20 @@ exports.getRepos = function(searchedName, key){
    var updated = moment.utc(response[i].updated_at).format("ddd MMM Do, YYYY");
     $('#results_list').append("<li><span class='repo_name'>"+ response[i].name + "</span> <ul><li> Last Updated: " + updated +"</li><li>" + description + "</li></ul></li>");
   }
+  })
+  .fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+};
+
+exports.getProfile = function(searchedName, key){
+  $.get('https://api.github.com/users/' + searchedName +'?access_token=' + key).then(function(response){
+    $('#user_profile').empty();
+    $('#results_list').empty();
+
+    $('#user_profile').append("<span class='repo_name'>Name:</span> " + response.name + "<br><span class='repo_name'>Location:</span> " + response.location + "<br><img src='"+ response.avatar_url +"'>");
+
   })
   .fail(function(error){
     console.log(error.responseJSON.message);
